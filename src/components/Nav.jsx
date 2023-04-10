c9cde709e14caac9fd25cb0156495cfefb5edce9
shivpreet <shivpreet16@gmail.com>
2023-04-10 16:01:19 +0530

    Added cursor pointer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ const Nav = () => {
     return (
         <nav className='absolute w-full apple text-xl mt-5'>
             <ul className='sm:flex justify-center gap-14 hidden'>
-                <li>Sponsors</li>
-                <li>Prizes</li>
-                <li>Timeline</li>
-                <li>Rules</li>
+                <li className=' cursor-pointer'>Sponsors</li>
+                <li className=' cursor-pointer'>Prizes</li>
+                <li className=' cursor-pointer'>Timeline</li>
+                <li className=' cursor-pointer'>Rules</li>
             </ul>
         </nav>
     )

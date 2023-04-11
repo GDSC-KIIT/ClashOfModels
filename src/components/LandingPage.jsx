504d6eacad04eeeae8d50f12f9e1f03f84b8be94
Jayanti2919 <jayanti2919@gmail.com>
2023-04-11 19:36:28 +0530

    added logos and fixed positions

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,8 +5,13 @@ const LandingPage = () => {
         <div className="h-screen w-full flex items-center justify-center sm:justify-start flex-col gap-8 sm:flex-row"
             style={{ background: 'radial-gradient(circle, #FFD969, #FABB03)' }}
         >
-            <h1 className='flex flex-col sm:ml-24 text-center sm:text-left'> <span className='text-3xl sm:text-6xl'> Clash Of </span><span className='text-5xl sm:text-9xl impact'>Models</span></h1>
-            <div className='sm:absolute bottom-[25vh] left-24 flex gap-8 text-xl'>
+            <div className='absolute top-24 flex gap-5'>
+            <img src="./gdsc.svg" alt="" className='ml-16 h-36 w-36'/>
+            <span className='relative top-14'>x</span>
+            <img src="./senseback.svg" alt="" className='h-36 w-36'/>
+            </div>
+            <h1 className='flex flex-col sm:ml-16 text-center sm:text-left'> <span className='text-3xl sm:text-6xl'> Clash Of </span><span className='text-5xl sm:text-9xl impact'>Models</span></h1>
+            <div className='sm:absolute bottom-[25vh] left-16 flex gap-8 text-xl'>
                 <h2>Strategize</h2>
                 <ul className=' list-disc flex gap-8'>
                     <li>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,13 @@ const Nav = () => {
     return (
         <nav className='absolute w-full apple text-xl mt-5'>
             <ul className='sm:flex justify-center gap-14 hidden'>
-                <li className=' cursor-pointer'>Sponsors</li>
-                <li className=' cursor-pointer'>Prizes</li>
-                <li className=' cursor-pointer'>Timeline</li>
-                <li className=' cursor-pointer'>Rules</li>
+                <div className='h-32 w-32 absolute left-16'>
+                <img src="./clashlogo.svg" alt="" />
+            </div>
+                <li className='navelements cursor-pointer'>Sponsors</li>
+                <li className='navelements cursor-pointer'>Prizes</li>
+                <li className='navelements cursor-pointer'>Timeline</li>
+                <li className='navelements cursor-pointer'>Rules</li>
             </ul>
         </nav>
     )

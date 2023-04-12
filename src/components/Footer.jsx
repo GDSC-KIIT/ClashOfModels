963e0decfe35b92705c9561aa5549d3894eeb56e
Jayanti2919 <jayanti2919@gmail.com>
2023-04-12 15:58:24 +0530

    Added responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,8 @@ import { motion } from "framer-motion";
 const Footer=()=>{
     return(
         <div className="py-4" style={{ background: "radial-gradient(circle, #FFD969, #FABB03)" }}>
-            <div className="top flex justify-between w-full px-10">
-                <img src={logo} alt="reward logo" className="w-[12rem]"/>
+            <div className="top flex justify-between items-center gap-10 w-full px-10">
+                <img src={logo} alt="reward logo" className="md:w-[12rem] w-[8rem]"/>
                 <div>
                     <p>powered by</p>
                     <img src={gdsc} alt="gdsc" className="h-[1.5rem] mt-2"/>
@@ -21,13 +21,13 @@ const Footer=()=>{
             <div className="flex justify-between w-full px-10 mt-8">
                 <p className="text-xs">© 2022 GDSC KIIT. All Rights Reserved.</p>
                 <div className="flex gap-6">
-                    <a href="#">
+                    <a href="https://www.linkedin.com/company/gdsckiit/">
                         <img src={lnkd} alt="linkedin" />
                     </a>
-                    <a href="#">
+                    <a href="https://www.instagram.com/gdsckiit/">
                         <img src={insta} alt="insta" />
                     </a>
-                    <a href="#">
+                    <a href="https://github.com/GDSC-KIIT">
                         <img src={github} alt="github" />
                     </a>
                     <a href="#">
--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -3,23 +3,20 @@ import React from "react";
 export const Rules = () => {
   return (
     <div className=" flex flex-col justify-center w-[100vw] mx-auto bg">
-      <h1 className="text-center text-glow text-5xl md:text-6xl font-semibold clash-bold headings pb-1">
+      <h1 className="text-center pt-5 text-glow text-5xl md:text-6xl font-semibold clash-bold headings pb-1">
         Rules
       </h1>
       <div className="mx-4 md:my-12">
-        <ul className="pl-10 text-xl font-Mori list-disc flex flex-col gap-4 max-w-3xl mx-auto leading-7 text-white/80">
+        <ul className="pl-10 pb-20 pt-10 text-xl pr-5 xs:leading-3 font-Mori list-disc flex flex-col gap-8 max-w-3xl mx-auto leading-7 text-white/80">
           <li>Participants are requested to show up at the venue on time.</li>
           <li>
           The session will start at 10AM, after which we will not be responsible for bringing any participants up to speed.
           </li>
           <li>
-          Participants are allowed to form teams of at max 3 people during the competition. Participants have to fill out a google form to let us know which participants are participating as a team and which participants are participating as a team.
+          Participants are allowed to form teams of at max 3 people during the competition. 
           </li>
           <li>
-          Participants are encouraged to treat this hackathon as a learning experience and take something positive away from the experience.
-          </li>
-          <li>
-          We at GDSC KIIT are trying to create a positive experience for everyone in this competition, and create an environment where everyone can learn more about artificial intelligence, and as such, it is a humble request to all participants to abide by the rules and help us in creating a positive experience for everybody.
+          Participants have to fill out a google form to let us know which participants are participating as a team and which participants are participating as a team.
           </li>
         </ul>
       </div>
